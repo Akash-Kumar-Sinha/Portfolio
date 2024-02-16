6e40ffca16881b42c430f22d7e333d876367702d
Akash-Kumar-Sinha <akashkumarsinha304@gmail.com>
2024-02-17 03:41:38 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import DesktopSidebar from "./Components/DesktopSidebar/DesktopSidebar"
+
+function App() {
+
+  return (
+    <h1 className="text-3xl bg-red-400 font-bold underline">
+      {/* 
+      <Header/>
+      <DesktopSidebar/>
+      <SideBar/>
+      <MobileSidebar/>
+      */}
+      Hello world!
+      <DesktopSidebar/>
+    </h1>
+  )
+}
+
+export default App
---
+++ src/Components/DesktopSidebar/DesktopSidebar.tsx
@@ -0,0 +1,37 @@
+const DesktopSidebar = () => {
+
+  return (
+    <>
+
+      <div>
+        <div
+          className="
+        hidden 
+        lg:fixed 
+        lg:inset-y-0 
+        lg:left-0 
+        lg:z-40 
+        lg:w-20 
+        xl:px-6
+        lg:overflow-y-auto 
+        lg:bg-aliceblue
+        lg:border-r-[1px]
+        lg:pb-4
+        lg:flex
+        lg:flex-col
+        justify-between
+    "
+        >
+          <nav className="mt-4 flex flex-col justify-between">
+            <ul className="flex flex-col items-center space-y-1">
+              
+            </ul>
+          </nav>
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DesktopSidebar;
